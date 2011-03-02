19df9da8cddaed21efd4a8af0886cce8838ff662
Markus Herhoffer <markus@herhoffer.net>
2011-03-02 12:48:23 +0100

    first commit

---
+++ bands/bands.php
@@ -0,0 +1,156 @@
+<?php
+/*
+Plugin Name: bands
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: "Bands" will generate a band portfolio for your festival, your agency or your network.
+Version: 0.1
+Author: Markus Herhoffer
+Author URI: http://d135-1r43.de
+License: GPL2
+*/
+
+register_activation_hook(__FILE__,'bands_install');
+add_action('init', 'add_band_types');
+add_action('add_meta_boxes', 'add_meta_boxes');
+
+global $bands_db_version;
+$bands_db_version = "1.0";
+
+function bands_install () {
+   global $wpdb;
+   global $band_db_version;
+
+   $table_name = $wpdb->prefix . "bands";
+   if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+      
+      $sql = "CREATE TABLE " . $table_name . " (
+	  id mediumint(9) NOT NULL AUTO_INCREMENT,
+	  time bigint(11) DEFAULT '0' NOT NULL,
+	  name tinytext NOT NULL,
+	  text text NOT NULL,
+	  website_url VARCHAR(55) NOT NULL,
+	  myspace_url VARCHAR(55) NOT NULL,
+	  facebook_page VARCHAR(55) NOT NULL,
+	  youtube_video_id VARCHAR(55) NOT NULL,
+	  UNIQUE KEY id (id)
+	);";
+
+      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+      dbDelta($sql);
+ 
+      add_option("band_db_version", $band_db_version);
+
+   }
+}
+
+function add_band_types () {
+
+	//register_taxonomy('bands-categories', 'bands', $args);
+
+	$args = array(
+		'labels' => array(
+			'name' => __('Bands'),
+	        'singular_name'	=> __('Band'),
+	        'add_new'       => __('Add band'),
+	        'add_new_item'  => __('Add band'),
+	        'new_item'      => __('Add band'),
+	        'view_item'     => __('View band'),
+	        'search_items'  => __('Search band'),
+	        'edit_item'     => __('Edit band'),
+	        'all_items'     => __('All bands'),
+	        'not_found'		=> __('No bands found'),
+	        'not_found_in_trash'    => __('No bands found in Trash'), 
+			'menu_name'		=> 'Bands'
+	        ),
+	 	'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+	 	'show_in_menu' => true,
+	 	'menu_position'         => 20,
+	 	'supports'=> array('title', 'editor'),
+	    'rewrite' => array('slug' => 'bands')
+	);
+	register_post_type('bands', $args);
+}
+
+function add_meta_boxes(){
+	add_meta_box('bands_social_links', 'Social Links', 'social_links_html', 'bands', 'side');
+	add_meta_box('bands_images', 'Images', 'images_html', 'bands', 'side');
+}
+
+function social_links_html() {
+      global $post;
+
+    // Noncename needed to verify where the data originated
+    echo '<input type="hidden" name="bands_membersmeta_noncename" id="bands_membersmeta_noncename" value="' .
+            wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+
+    // Get the location data if its already been entered
+	$video_id = get_post_meta($post->ID, '_video_id', true);
+    $twitter = get_post_meta($post->ID, '_twitter', true);
+    $facebook = get_post_meta($post->ID, '_facebook', true);
+    $flickr = get_post_meta($post->ID, '_flickr', true);
+    $youtube = get_post_meta($post->ID, '_youtube', true);
+    $myspace = get_post_meta($post->ID, '_myspace', true);
+
+    // Echo out the field
+	echo '<p>Music Video ID on YouTube</p>';
+    echo '<input type="text" name="_video_id" value="' . $video_id  . '" class="widefat" />';
+    echo '<p>Twitter Username</p>';
+    echo '<input type="text" name="_twitter" value="' . $twitter  . '" class="widefat" />';
+    echo '<p>FaceBook Username</p>';
+    echo '<input type="text" name="_facebook" value="' . $facebook  . '" class="widefat" />';
+    echo '<p>Flickr Username</p>';
+    echo '<input type="text" name="_flickr" value="' . $flickr  . '" class="widefat" />';
+    echo '<p>YouTube Username</p>';
+    echo '<input type="text" name="_youtube" value="' . $youtube  . '" class="widefat" />';
+    echo '<p>MySpace Username</p>';
+    echo '<input type="text" name="_myspace" value="' . $myspace  . '" class="widefat" />'; 
+}
+
+function images_html(){
+	echo '<p><label for="upload_image">Band-Logo</label></p>';
+	echo '<input id="upload_image" type="text" size="36" name="upload_image" value="" />';
+	echo '<input id="upload_image_button" type="button" value="Upload Image" />';
+}
+
+function bands_save_members_meta($post_id, $post) {
+ 
+        // verify this came from the our screen and with proper authorization,
+        // because save_post can be triggered at other times
+        if ( !wp_verify_nonce( $_POST['bands_membersmeta_noncename'], plugin_basename(__FILE__) )) {
+        return $post->ID;
+        }
+ 
+        // Is the user allowed to edit the post or page?
+        if ( !current_user_can( 'edit_post', $post->ID ))
+                return $post->ID;
+ 
+        // OK, we're authenticated: we need to find and save the data
+        // We'll put it into an array to make it easier to loop though.
+ 
+        $members_meta['_video_id'] = $_POST['_video_id'];
+		$members_meta['_twitter'] = $_POST['_twitter'];
+        $members_meta['_facebook'] = $_POST['_facebook'];
+        $members_meta['_flickr'] = $_POST['_flickr'];
+        $members_meta['_youtube'] = $_POST['_youtube'];
+        $members_meta['_wordpress'] = $_POST['_wordpress'];
+        $members_meta['_myspace'] = $_POST['_myspace'];
+        $members_meta['_vetitleurl'] = $_POST['_vetitleurl'];
+   
+        // Add values of $members_meta as custom fields
+ 
+        foreach ($members_meta as $key => $value) { // Cycle through the $members_meta array!
+                if( $post->post_type == 'revision' ) return; // Don't store custom data twice
+                $value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
+                if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
+                        update_post_meta($post->ID, $key, $value);
+                } else { // If the custom field doesn't have a value
+                        add_post_meta($post->ID, $key, $value);
+                }
+                if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
+        } 
+}
+
+add_action('save_post', 'bands_save_members_meta', 1, 2); // save the custom fields
+?>
