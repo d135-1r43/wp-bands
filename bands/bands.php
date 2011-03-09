8a1271ac11c8632eb48748f199eb80919b103216
Markus Herhoffer <markus@herhoffer.net>
2011-03-09 15:00:05 +0100

    genre taxonomy added

--- bands/bands.php
+++ bands/bands.php
@@ -48,6 +48,29 @@ function add_band_types () {
     	'rewrite' => array( 'slug' => 'event' ),
   	));
 
+	// genre taxonomy
+	$labels = array(
+	    'name' => _x( 'Genres', 'taxonomy general name' ),
+	    'singular_name' => _x( 'Genre', 'taxonomy singular name' ),
+	    'search_items' =>  __( 'Search Genres' ),
+	    'all_items' => __( 'All Genres' ),
+	    'parent_item' => __( 'Parent Genre' ),
+	    'parent_item_colon' => __( 'Parent Genre:' ),
+	    'edit_item' => __( 'Edit Genre' ), 
+	    'update_item' => __( 'Update Genre' ),
+	    'add_new_item' => __( 'Add New Genre' ),
+	    'new_item_name' => __( 'New Genre Name' ),
+	    'menu_name' => __( 'Genres' ),
+	  ); 	
+
+	  register_taxonomy('genre',array('bands'), array(
+	    'hierarchical' => false,
+	    'labels' => $labels,
+	    'show_ui' => true,
+	    'query_var' => true,
+	    'rewrite' => array( 'slug' => 'genre' ),
+	  ));
+
 	// post type for bands
 	$args = array(
 		'labels' => array(
